307c39d4eb8921f36d7ae2aa1a7c5c991f226d4f
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-07-17 19:40:49 +0600

    зафиксировали изменнения второй строки

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Мы изучаем версионность GitHab");
+        System.out.println("Добавил строку для демострации");
     }
 }
