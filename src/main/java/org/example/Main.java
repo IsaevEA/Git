81404436fdab2fc2dcdbae376cd5bfba77c422b2
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-07-17 20:28:19 +0600

    тест первый

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Мы изучаем версионность GitHab");
         System.out.println("Chtpfsd;lgk строку для демострации");
+        System.out.println("Chtpfsd;lgk строку для демострации");
+        System.out.println("Chtpfsd;lgk строку для демострации");
+        System.out.println("Chtpfsd;lgk строку для демострации");
+        System.out.println("Chtpfsd;lgk строку для демострации");
+        System.out.println("Chtpfsd;lgk строку для демострации");
     }
 }
